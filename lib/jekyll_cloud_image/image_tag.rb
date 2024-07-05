a43192143f6780c6f5f7653165cde3e7c4e4ab25
cloudqubes <cloud.qubes@gmail.com>
2024-07-05 16:34:15 +0530

    updated text format

--- lib/jekyll_cloud_image/image_tag.rb
+++ lib/jekyll_cloud_image/image_tag.rb
@@ -5,12 +5,14 @@ module Jekyll
       def initialize(tag_name, text, tokens) 
         super
         @image_file = text
+        @tokens = tokens
       end
 
       def render(context)
         @settings = context.registers[:site].config["image_url_prefix"]
         # "<img src=#{@image_file}>"
-        return "<img src=\"#{@image_file}\" alt=\"Girl in a jacket\" width=\"500\" height=\"600\">"
+        # return "<img src=\"#{@image_file}\" alt=\"Girl in a jacket\" width=\"500\" height=\"600\">"
+        return @tokens
       end
     end
   end
