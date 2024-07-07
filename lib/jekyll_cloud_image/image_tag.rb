c97f0c3e294afc0191d9ff40183b410e0b1f05c0
cloudqubes <cloud.qubes@gmail.com>
2024-07-08 04:09:59 +0530

    updated text format

--- lib/jekyll_cloud_image/image_tag.rb
+++ lib/jekyll_cloud_image/image_tag.rb
@@ -21,8 +21,10 @@ module Jekyll
       def render(context)
         @settings = context.registers[:site].config["image_url_prefix"]
         alter_text = convert_key_to_string(:alt)
+        width = convert_key_to_string(:width)
+        height = convert_key_to_string(:height)
         # "<img src=#{@image_file}>"
-        return "<img src=\"#{@image_file}\" #{alter_text} width=\"500\" height=\"600\">"
+        return "<img src=\"#{@image_file}\" #{alter_text} #{width} #{height}>"
       end
 
       def get_key_and_value(param)
