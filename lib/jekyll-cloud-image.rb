1581acc6fb2c48b726d80ab600684ac77a432f02
cloudqubes <cloud.qubes@gmail.com>
2024-07-09 04:54:04 +0530

    updated text format

--- lib/jekyll-cloud-image.rb
+++ lib/jekyll-cloud-image.rb
@@ -1,5 +1,6 @@
 require_relative 'jekyll_cloud_image/converter'
 require_relative 'jekyll_cloud_image/image_tag'
+require_relative 'jekyll_cloud_image/commands/setup'
 
 # module JekyllCloudImage
 #   class << self
