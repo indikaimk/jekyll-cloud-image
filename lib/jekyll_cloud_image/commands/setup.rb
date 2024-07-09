cb2bd2224cd4c6debc77847be2e432e2d56c3f5a
cloudqubes <cloud.qubes@gmail.com>
2024-07-10 04:55:59 +0530

    updated text format

--- lib/jekyll_cloud_image/commands/setup.rb
+++ lib/jekyll_cloud_image/commands/setup.rb
@@ -29,7 +29,7 @@ module Jekyll
         def update_git_ignore_file 
           puts "Adding entries to the .gitignore file"
           git_ignore = File.open('.gitignore', 'a')
-          git_ignore.write('_cloud_images')
+          git_ignore.write('_cloud_images\n')
           git_ignore.close          
         end
 
