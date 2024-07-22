51ed19a8632589b5a22c7f7caa9721c4e520dd4d
cloudqubes <cloud.qubes@gmail.com>
2024-07-22 05:30:25 +0530

    update

--- lib/jekyll_cloud_image/converter.rb
+++ lib/jekyll_cloud_image/converter.rb
@@ -1,40 +1,42 @@
 require 'vips'
 
-module JekyllCloudImage
+module Jekyll
+  module CloudImage
   
-  class Converter
-    
-    # def initialize(source, destination, type: webp)
-    #   @source = source
-    #   @destination = source
-    #   @type = :webp
-    # end
-    
-    # Convert png/jpg image to webp
-    def self.convert(source)
-      puts source
-      if File.directory? source
-        Dir.each_child(source) do |filename|
-          puts filename
-          if filename.end_with? ".png" or filename.end_with? ".jpeg"
-            self.convert_file source + "/" + filename            
-          end
-
-        end     
-      else
-        self.convert_file source
-      end
-
+    class Converter
       
+      # def initialize(source, destination, type: webp)
+      #   @source = source
+      #   @destination = source
+      #   @type = :webp
+      # end
+      
+      # Convert png/jpg image to webp
+      def self.convert(source)
+        puts source
+        if File.directory? source
+          Dir.each_child(source) do |filename|
+            puts filename
+            if filename.end_with? ".png" or filename.end_with? ".jpeg"
+              self.convert_file source + "/" + filename            
+            end
 
+          end     
+        else
+          self.convert_file source
+        end
+
+        
+
+      end
+      
+      private
+        def self.convert_file(filename)
+          webp_filename = filename.chomp(File.extname filename) + '.webp'
+          im = Vips::Image.new_from_file filename
+          im.write_to_file webp_filename      
+        
+        end
     end
-    
-    private
-      def self.convert_file(filename)
-        webp_filename = filename.chomp(File.extname filename) + '.webp'
-        im = Vips::Image.new_from_file filename
-        im.write_to_file webp_filename      
-      
-      end
   end
 end
